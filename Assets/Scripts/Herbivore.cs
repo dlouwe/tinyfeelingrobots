f1e284dfd6084778095140a4a1f75c037c1601bb
Daniel Sunshine <dlouwe@gmail.com>
2019-11-03 17:51:13 -0800

    latest updates

---
+++ Assets/Scripts/Herbivore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Herbivore : Entity {
+  
+  public override GameObject AttemptGrow() {
+
+    return null;
+    
+  }
+
+  public override void _Update() {
+
+    pathfindTarget = GetClosestTag("Grass", 6f, false);
+
+    if (pathfindTarget != null) {
+      MoveToTarget();
+    }
+
+  }
+
+  // Use this for initialization
+  // void Start () {
+    
+  // }
+  
+  // Update is called once per frame
+  // void Update () {
+    
+  // }
+}
